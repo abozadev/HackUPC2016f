7f4c3bcb0b40e7825773984e324c918fa9ca8aab
eloimuns <eloimuns@gmail.com>
2016-10-08 04:26:09 +0200

    We've tabs, IOS style

---
+++ ionic/www/js/routes.js
@@ -0,0 +1,54 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+  .state('tabsController.cars', {
+    url: '/cars',
+    views: {
+      'tab1': {
+        templateUrl: 'views/cars.html',
+        controller: 'carsCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.cart', {
+    url: '/containers',
+    views: {
+      'tab2': {
+        templateUrl: 'views/drugs.html',
+        controller: 'drugsCtrl'
+      }
+    }
+  })
+
+
+  .state('tabsController.terrorism', {
+    url: '/terrorism',
+    views: {
+      'tab3': {
+        templateUrl: 'views/terrorism.html',
+        controller: 'terrorismCtrl'
+      }
+    }
+  })
+
+  .state('tabsController', {
+    url: '/main',
+    templateUrl: 'views/tabsController.html',
+    abstract:true
+  })
+
+$urlRouterProvider.otherwise('/main/cars')
+
+  
+
+});
